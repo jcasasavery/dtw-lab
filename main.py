c885f265511d9292eb7bf3a9b82287d53bfcee43
jcasasavery <202011032@alu.comillas.edu>
2025-02-04 18:20:58 +0100

    adding clean data to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.dtw_lab.lab1 import read_csv_from_google_drive, visualize_data, calculate_statistic
-
+from src.dtw_lab.lab1 import read_csv_from_google_drive,visualize_data, calculate_statistic, clean_data
 
 
 if __name__=='__main__':
